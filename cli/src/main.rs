2d50bd164563df1164624b20096dd18d75657b84
nukr <me@n-u.kr>
2023-02-21 14:35:27 +0800

    add send_auth_code_email

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use marketplace::{Context, SendAuthCodeEmailCommand, Service};
+
+#[tokio::main]
+async fn main() {
+    let context =
+        Context::new("postgres://postgres:duf3QMY!tma!dfp3wcp@35.221.167.63/postgres").unwrap();
+    let command = SendAuthCodeEmailCommand {
+        to: String::from("me@n-u.kr"),
+    };
+    context.send_auth_code_email(command).await.unwrap();
     println!("Hello, world!");
 }
--- marketplace/src/lib.rs
+++ marketplace/src/lib.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use domain::EmailCode;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use thiserror::Error;
 use uuid::Uuid;
@@ -45,7 +46,59 @@ pub trait HaveUserRepository {
 }
 
 #[async_trait]
-pub trait Service: HaveAuthnRepository + HaveUserRepository {
+pub trait SettingRepository {
+    async fn retrive_setting(
+        &self,
+        key: SettingKey,
+    ) -> Result<Option<String>, SettingRepositoryError>;
+}
+
+pub trait HaveSettingRepository {
+    type Repository: SettingRepository + Sync;
+    fn provide_setting_repository(&self) -> &Self::Repository;
+}
+
+pub struct SendAuthCodeEmailCommand {
+    pub to: String,
+}
+pub struct ComfirmAuthCodeEmailCommand {}
+
+pub enum SettingKey {
+    SendGridApiKey,
+}
+
+impl ToString for SettingKey {
+    fn to_string(&self) -> String {
+        match self {
+            SettingKey::SendGridApiKey => "send_grid_api_key".to_string(),
+        }
+    }
+}
+
+#[async_trait]
+pub trait Service: HaveAuthnRepository + HaveUserRepository + HaveSettingRepository {
+    async fn send_auth_code_email(
+        &self,
+        command: SendAuthCodeEmailCommand,
+    ) -> Result<(), CoreServiceError> {
+        let setting_repo = self.provide_setting_repository();
+        let send_grid_api_key = setting_repo
+            .retrive_setting(SettingKey::SendGridApiKey)
+            .await
+            .unwrap()
+            .unwrap();
+        let mailer = sendgrid::SendGrid::new(&send_grid_api_key);
+        mailer
+            .send_mail_code(EmailCode::from_email(&command.to).unwrap())
+            .await;
+        Ok(())
+    }
+    async fn comfirm_auth_code_email(
+        &self,
+        _command: ComfirmAuthCodeEmailCommand,
+    ) -> Result<(), CoreServiceError> {
+        unimplemented!()
+    }
     async fn get_user(&self, session_id: SessionID) -> Result<User, CoreServiceError> {
         let authn_repo = self.provide_authn_repository();
         let user_id = authn_repo.auth(session_id).await?;
@@ -67,7 +120,7 @@ pub trait Service: HaveAuthnRepository + HaveUserRepository {
     }
 }
 
-impl<T: HaveAuthnRepository + HaveUserRepository> Service for T {}
+impl<T: HaveAuthnRepository + HaveUserRepository + HaveSettingRepository> Service for T {}
 
 #[derive(Error, Debug)]
 pub enum CoreServiceError {
@@ -75,6 +128,8 @@ pub enum CoreServiceError {
     AuthenticationError(#[from] AuthnRepositoryError),
     #[error("user error")]
     UserError(#[from] UserRepositoryError),
+    #[error("setting error")]
+    SettingError(#[from] SettingRepositoryError),
 }
 
 #[derive(Error, Debug)]
@@ -89,6 +144,16 @@ pub enum AuthnRepositoryError {
 
 #[derive(Error, Debug)]
 pub enum UserRepositoryError {
+    #[error("Uuid parse error")]
+    UUIDParseError(#[from] uuid::Error),
+    #[error("SQLx error")]
+    SQLxError(#[from] sqlx::Error),
+    #[error("Unauthenticated")]
+    Unauthenticated,
+}
+
+#[derive(Error, Debug)]
+pub enum SettingRepositoryError {
     #[error("Uuid parse error")]
     UUIDParseError(#[from] uuid::Error),
     #[error("SQLx error")]
@@ -150,6 +215,20 @@ impl UserRepository for Context {
     }
 }
 
+#[async_trait]
+impl SettingRepository for Context {
+    async fn retrive_setting(
+        &self,
+        key: SettingKey,
+    ) -> Result<Option<String>, SettingRepositoryError> {
+        let record = sqlx::query!("SELECT v FROM settings WHERE k = $1", key.to_string())
+            .fetch_one(&self.pool)
+            .await?;
+        let v = record.v;
+        Ok(Some(v))
+    }
+}
+
 impl HaveUserRepository for Context {
     type Repository = Context;
     fn provide_user_repository(&self) -> &Self::Repository {
@@ -161,4 +240,145 @@ impl HaveAuthnRepository for Context {
     fn provide_authn_repository(&self) -> &Self::Repository {
         self
     }
+}
+
+impl HaveSettingRepository for Context {
+    type Repository = Context;
+    fn provide_setting_repository(&self) -> &Self::Repository {
+        self
+    }
+}
+
+mod sendgrid {
+    use reqwest::{
+        header::{HeaderMap, HeaderValue, AUTHORIZATION},
+        Method,
+    };
+    use serde::Serialize;
+
+    use crate::domain::{EmailAddress, EmailCode};
+
+    #[derive(Serialize)]
+    pub struct SendEmailData {
+        personalizations: Vec<Personalization>,
+    }
+
+    #[derive(Serialize)]
+    pub struct SenderData {
+        email: EmailAddress,
+        subject: String,
+        content: Vec<EmailContent>,
+    }
+
+    #[derive(Serialize)]
+    pub struct EmailContent {
+        r#type: String,
+        value: String,
+    }
+
+    #[derive(Serialize)]
+    pub struct Personalization {
+        to: Vec<String>,
+        from: SenderData,
+    }
+
+    pub struct SendGrid {
+        key: String,
+    }
+
+    impl SendGrid {
+        pub fn new(key: &str) -> Self {
+            Self {
+                key: key.to_string(),
+            }
+        }
+        pub async fn send_mail_code(&self, email_code: EmailCode) {
+            let url = "https://api.sendgrid.com/v3/mail/send";
+            let data = SendEmailData {
+                personalizations: vec![Personalization {
+                    to: vec![email_code.get_address().to_string()],
+                    from: SenderData {
+                        email: EmailAddress::parse("noreply@tcgbuysell.com").unwrap(),
+                        subject: String::from("Sign in to TCG Marketplace"),
+                        content: vec![EmailContent {
+                            r#type: "text/plain".to_string(),
+                            value: String::from(email_code.get_code()),
+                        }],
+                    },
+                }],
+            };
+            let json_data = serde_json::to_string(&data).unwrap();
+            let client = reqwest::Client::new();
+            let mut headers = HeaderMap::new();
+            let authorization_value =
+                HeaderValue::from_str(&format!("Bearer {}", self.key)).unwrap();
+            headers.insert(AUTHORIZATION, authorization_value);
+            headers.insert("Content-Type", HeaderValue::from_static("application/json"));
+            let result = client
+                .request(Method::POST, url)
+                .headers(headers)
+                .body(json_data)
+                .send()
+                .await
+                .unwrap();
+        }
+    }
+}
+mod domain {
+    use rand::{distributions::Alphanumeric, Rng};
+    use serde::Serialize;
+    use thiserror::Error;
+
+    pub struct EmailCode {
+        email: EmailAddress,
+        code: Code,
+    }
+
+    impl EmailCode {
+        pub fn from_email(email: &str) -> Result<Self, AuthnError> {
+            let email = EmailAddress::parse(email)?;
+            Ok(Self {
+                email,
+                code: Code::new(),
+            })
+        }
+        pub fn comfirm_code(&self, code: &str) -> bool {
+            self.code.comfirm(code)
+        }
+        pub fn get_address(&self) -> &str {
+            &self.email.0
+        }
+        pub fn get_code(&self) -> &str {
+            &self.code.get_code()
+        }
+    }
+
+    #[derive(Serialize)]
+    pub struct EmailAddress(String);
+
+    impl EmailAddress {
+        pub fn parse(s: &str) -> Result<Self, AuthnError> {
+            Ok(Self(s.to_string()))
+        }
+    }
+    struct Code(String);
+    impl Code {
+        fn new() -> Self {
+            let rand_str: String = rand::thread_rng()
+                .sample_iter(Alphanumeric)
+                .take(6)
+                .map(char::from)
+                .collect();
+            Self(rand_str)
+        }
+        fn comfirm(&self, code: &str) -> bool {
+            code.to_string() == self.0
+        }
+        fn get_code(&self) -> &str {
+            &self.0
+        }
+    }
+
+    #[derive(Debug, Error)]
+    pub enum AuthnError {}
 }
