7fd23a6d4014153fc90cda1cc677c5c04d2f6e8b
nukr <me@n-u.kr>
2023-02-25 16:31:07 +0800

    eliminated warnings from cli package

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,18 +1,23 @@
-use marketplace::{
-    ConfirmAuthCodeEmailCommand, Context, SendAuthCodeEmailCommand, Service, SessionID,
-    StartSessionCommand,
-};
-
 #[tokio::main]
-async fn main() {
+async fn main() {}
+
+pub async fn send() {
+    use marketplace::{Context, SendAuthCodeEmailCommand, Service, StartSessionCommand};
     let context =
         Context::new("postgres://postgres:duf3QMY!tma!dfp3wcp@35.221.167.63/postgres").unwrap();
-    // let session_id = context.start_session(StartSessionCommand {}).await.unwrap();
-    // let command = SendAuthCodeEmailCommand {
-    //     to: String::from("me@n-u.kr"),
-    //     session_id,
-    // };
-    // context.send_auth_code_email(command).await.unwrap();
+    let session_id = context.start_session(StartSessionCommand {}).await.unwrap();
+    let command = SendAuthCodeEmailCommand {
+        to: String::from("me@n-u.kr"),
+        session_id,
+    };
+    context.send_auth_code_email(command).await.unwrap();
+    println!("Hello, world!");
+}
+
+pub async fn confirm() {
+    use marketplace::{ConfirmAuthCodeEmailCommand, Context, Service, SessionID};
+    let context =
+        Context::new("postgres://postgres:duf3QMY!tma!dfp3wcp@35.221.167.63/postgres").unwrap();
     let command = ConfirmAuthCodeEmailCommand {
         session_id: SessionID::parse("ecbff5dc-a763-4772-b194-c6a5ec310a6c").unwrap(),
         code: String::from("pQy08v"),
